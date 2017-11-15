2a1c5ced820eb227d26decd6c1c2b6d858f0a712
David <lexi.brown@rogers.com>
2017-11-14 20:25:04 -0500

    Project setup

---
+++ src/main/java/com/lexi/comp4004/server/StartApplication.java
@@ -0,0 +1,19 @@
+package com.lexi.comp4004.server;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class StartApplication extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		// register root resource
+		
+		classes.add(Hello.class);
+		return classes;
+	}
+}
